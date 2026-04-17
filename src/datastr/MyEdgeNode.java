617878a80fc165250bf0c2d90cdba93e4034dbaf
s25abelmiku <michisillproductions@gmail.com>
2026-04-17 19:20:46 +0300

    Start

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,5 +1,47 @@
 package datastr;
 
 public class MyEdgeNode {
+	private int indexOfVerticeTo;
+	private int weight;
+	private MyEdgeNode nextEdgeNode = null;
 
+	public int getIndexOfVerticeTo() {
+		return indexOfVerticeTo;
+	}
+
+	public void setIndexOfVerticeTo(int indexOfVerticeTo) {
+		if(indexOfVerticeTo >= 0) {
+		this.indexOfVerticeTo = indexOfVerticeTo;
+	}else {
+		this.indexOfVerticeTo = 0;
+	}
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		if(weight > 0) {
+		this.weight = weight;
+		}else {
+			this.weight = 1;
+		}
+	}
+
+	public MyEdgeNode getNextEdgeNode() {
+		return nextEdgeNode;
+	}
+
+	public void setNextEdgeNode(MyEdgeNode nextEdgeNode) {
+		this.nextEdgeNode = nextEdgeNode;
+	}
+	public MyEdgeNode(int indexOfVerticeTo, int weight) {
+		setIndexOfVerticeTo(indexOfVerticeTo);
+		setWeight(weight);
+		
+	}
+	public String toString() {
+		return "index";
+	}
 }
